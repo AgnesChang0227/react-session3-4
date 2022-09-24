0cb8937924791d7482ea299a5d39d9f24f92e8b6
agneschang <agneschang0227@gmail.com>
2022-09-24 20:20:36 +0800

    create一個button，加上onClick 的event，和其callback fn在ExprenseItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ExpenseDate from "./ExpenseDate";//import separate component
 import Card from "../UI/Card";
-import "./ExpenseItem.css";//import css
+import "./ExpenseItem.css";
+import {click} from "@testing-library/user-event/dist/click";
+//import css
 
 //app.js 會傳送一個object過來 => props
 const ExpenseItem = (props) => {
+    const clickHandler=()=>{//Event fn
+        console.log("Clicked!")
+    }
     return (
         //使用card作為html element
         <Card className="expense-item">
@@ -14,6 +19,8 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            {/*Event: onClick={...fn}*/}
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     )
 }
