0e0c808277dc499a8c1f085de356f241459c5aae
agneschang <agneschang0227@gmail.com>
2022-09-24 20:52:26 +0800

    使用hook:useState=>令event 發生時改變state，state改變時會refresh component，使value在頁面上改變

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,26 @@
-import React from "react";
+import React,{useState} from "react";//import useState
+
 import ExpenseDate from "./ExpenseDate";//import separate component
 import Card from "../UI/Card";
-import "./ExpenseItem.css";
-import {click} from "@testing-library/user-event/dist/click";
-//import css
+import "./ExpenseItem.css";//import css
 
 //app.js 會傳送一個object過來 => props
 const ExpenseItem = (props) => {
+    //hooks，只能在react component fn 內 call
+    //useState: 當state改變時，refresh這個component
+    //語法：const [state name, fn] = useState(value)
+    const [title,setTitle] = useState(props.title);
     const clickHandler=()=>{//Event fn
-        console.log("Clicked!")
+        setTitle("updated!");//update 新value
     }
+
     return (
         //使用card作為html element
         <Card className="expense-item">
             {/*放入component*/}
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
             {/*Event: onClick={...fn}*/}
