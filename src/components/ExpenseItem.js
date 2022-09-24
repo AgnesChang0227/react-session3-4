834d4f3adea416c7cb0399acbcb7d255d82f8fed
agneschang <agneschang0227@gmail.com>
2022-09-24 15:21:20 +0800

    將app.js 的props 傳送到另一個components,變成一個object並使用

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,17 @@
 import "./ExpenseItem.css";//import css
 
-const ExpenseItem = () => {
-    //JS code
-    const expenseDate = new Date(2021,2,28);
-    const expenseTitle = "Car Insurance";
-    const expenseAmount = 294.67;
-
+//app.js 會傳送一個object過來 => props
+const ExpenseItem = (props) => {
     return (//return沒有()就只會return 一行
         //在div配置className，以使用css中的style
         <div className="expense-item">
-            {/*object 要先變成string 再放入{}*/}
-            <div>{expenseDate.toDateString()}</div>
+            {/*在{}中放入props.attribute*/}
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
                 {/*用{}放入JS code => string  */}
-                <h2>{expenseTitle}</h2>
+                <h2>{props.title}</h2>
                 {/*放入number的話 要用${} */}
-                <div className="expense-item__price">${expenseAmount}</div>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     )//因此要用()括起來
