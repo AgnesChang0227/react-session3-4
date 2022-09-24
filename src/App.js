29928aaa96321e3584461b337e86671c3724a976
agneschang <agneschang0227@gmail.com>
2022-09-24 14:35:51 +0800

    create a component=> export and import in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+//import other components
+import ExpenseItem from "./components/ExpenseItem";
+
+//Main component
 function App(){
   return(
+      //JSX => JS XML => HTML(XML) in JS
       <div>
         <h2>Let's get started!</h2>
+          {/*放置import的components*/}
+          <ExpenseItem/>
       </div>
   )
 }
