86eaf01e1584d983f8c8473be545c25ed37ece0d
agneschang <agneschang0227@gmail.com>
2022-09-24 14:45:51 +0800

    create css file, download the example and import in ExpenseItem, using by className

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,15 @@
+import "./ExpenseItem.css";//import css
 
 const ExpenseItem = () => {
-    return (
-        <div>
-            <h2>Expense Item</h2>
+    return (//return沒有()就只會return 一行
+        //在div配置className，以使用css中的style
+        <div className="expense-item">
+            <div>Date</div>
+            <div className="expense-item__description">
+                <h2>Title</h2>
+                <div className="expense-item__price">Amount</div>
+            </div>
         </div>
-    )
+    )//因此要用()括起來
 }
-export default ExpenseItem ;
+export default ExpenseItem;
